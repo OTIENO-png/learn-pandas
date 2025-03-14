d3e9b3f64f60f1b3bdbfce82eba0231b999cbb13
brian <otienolubalo@gmail.com>
2025-03-14 22:29:58 +0300

    First file of panda about pandas Series with matching data and index lengths

--- new.py
+++ new.py
@@ -1,1 +1,11 @@
-First file for learning about pandas 
+import pandas as pd
+
+# Check pandas version
+print(pd.__version__)
+
+# Create a pandas Series with matching data and index lengths
+my_series = pd.Series([10, 20, 30, 56, 96, 200], index=["a", "b", "c", "d", "e", "f"])
+
+# Display the Series
+#type(my_series)
+print(my_series)
